3e1b7a0f49c2d53533b1e91c44ce3376254fea27
Anmakulaku <anmakulaku@gmail.com>
2023-12-19 15:44:07 +0100

    Add grid to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,25 +12,29 @@ export function Header() {
             <div className="header__container">
             <div className="header__grid">
                 <div className="header__grid-1">
-                    <div id="img1" className="lil-overlay"></div>
-                    {/* <img src={header1} alt="img1" /> */}
+                    <div className="header__img-big">
+                        <img src={header1} alt="img1" className='img1' />
+                    </div>
                 </div>
                 <div className='header__grid-middle'>
                     <div className="header__grid-2">
-                        <div id="img2" className="lil-overlay"></div>
-                        {/* <img src={header2} alt="img2" /> */}
+                        <div id="img2" className="header__img-small">
+                            <img src={header2} alt="img2" className='img2'/>
+                        </div>
                     </div>
                     <div className="header__grid-text">
                         <span>ULTIMATE SALE</span>
                     </div>
                     <div className="header__grid-3">
-                        <div id="img3" className="lil-overlay"></div>
-                        {/* <img src={header3} alt="img3" /> */}
+                        <div id="img3" className="header__img-small">
+                            <img src={header3} alt="img3" className='img3'/>
+                        </div>
                     </div>
                 </div>
                 <div className="header__grid-4">
-                    <div id="img4" className="lil-overlay"></div>
-                    {/* <img src={header4} alt="img4" /> */}
+                    <div className="header__img-big">
+                        <img src={header4} alt="img4" className='img4' />
+                    </div>
                 </div>
             </div>
             </div>
