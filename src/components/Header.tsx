b6724b5aa50dc9df4b42eecbf8e50e97ccbbe679
Anmakulaku <anmakulaku@gmail.com>
2023-12-20 11:55:57 +0100

    Grid style

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,30 +11,22 @@ export function Header() {
         <div className="header">
             <div className="header__container">
             <div className="header__grid">
-                <div className="header__grid-1">
-                    <div className="header__img-big">
-                        <img src={header1} alt="img1" className='img1' />
-                    </div>
+                <div className="header__grid-1 header__img-big">
+                    <img src={header1} alt="img1" className='img1' />
                 </div>
                 <div className='header__grid-middle'>
-                    <div className="header__grid-2">
-                        <div id="img2" className="header__img-small">
-                            <img src={header2} alt="img2" className='img2'/>
-                        </div>
+                    <div className="header__grid-2 header__img-small">
+                        <img src={header2} alt="img2" className='img2'/>
                     </div>
                     <div className="header__grid-text">
                         <span>ULTIMATE SALE</span>
                     </div>
-                    <div className="header__grid-3">
-                        <div id="img3" className="header__img-small">
-                            <img src={header3} alt="img3" className='img3'/>
-                        </div>
+                    <div className="header__grid-3 header__img-small">
+                        <img src={header3} alt="img3" className='img3'/>
                     </div>
                 </div>
-                <div className="header__grid-4">
-                    <div className="header__img-big">
-                        <img src={header4} alt="img4" className='img4' />
-                    </div>
+                <div className="header__grid-4 header__img-big">
+                    <img src={header4} alt="img4" className='img4' />
                 </div>
             </div>
             </div>
