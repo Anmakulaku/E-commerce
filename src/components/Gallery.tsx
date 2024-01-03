0da3c332a8bbeafd8be8174afd7243408a9462f0
Anmakulaku <anmakulaku@gmail.com>
2024-01-03 13:33:47 +0100

    Fix gallery bugs

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,9 +1,9 @@
 import './Gallery.css'
-import gallery1 from "../img/gallery1.jpg"
+import gallery1 from "../img/gallery1.png"
 import gallery2 from "../img/gallery2.jpg"
-import gallery3 from "../img/gallery3.jpg"
+import gallery3 from "../img/gallery3.png"
 import gallery4 from "../img/gallery4.jpg"
-import gallery5 from "../img/gallery5.jpg"
+import gallery5 from "../img/gallery5.png"
 import gallery6 from "../img/gallery6.png"
 import gallery7 from "../img/gallery7.png"
 
@@ -18,8 +18,8 @@ export function Gallery() {
                 <img src={gallery3} alt="woman in shorts" className='gallery__small'/>
                 <img src={gallery4} alt="man in red trousers" className='gallery__big'/>
                 <img src={gallery5} alt="woman in blue dress" className='gallery__small'/>
-                <img src={gallery6} alt="man in a jacket" className='gallery__big'/>
-                <img src={gallery7} alt="woman in pink dress" className='gallery__small'/>
+                <img src={gallery6} alt="man in a jacket" className='gallery__big '/>
+                <img src={gallery7} alt="woman in pink dress" className='gallery__small gallery7'/>
             </div>
         </div>
 )}
