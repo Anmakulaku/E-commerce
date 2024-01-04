6a227670bcb6e151308731150cfc4f0a8bd3b43e
Anmakulaku <anmakulaku@gmail.com>
2024-01-04 11:46:51 +0100

    Add Styles for Slider

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -4,15 +4,16 @@ import slider1 from "../img/slider1.png"
 export function Slider() {
     return(
         <div className='slider section__margin'>
-            <div className='slider__content'>
-                <div className='slider__grid'>
-                    <div className='slider__imageBox'></div>
-                        <img src={slider1} alt="Peaky Blinders woman"/>
-                    <div className='slider__separatorBox'></div>
-                    <div className='slider__textBox'></div>
-                        <h2>Peaky Blinders</h2>
-                        <p>Discover a unique style inspired by the world of Peaky Blinders! Choose elegance and class to create your own distinctive look today!</p>
-                        <button className='button slider__btn'> Shop Now </button>
+            <div className='slider__grid'>
+                <div className='slider__imageBox'>
+                    <img src={slider1} alt="Peaky Blinders woman"/>
+                    </div>
+                <div className='slider__separatorBox'></div>
+                <div className='slider__textBox'>
+                    <p className='slider__description'>Women Collection</p>
+                    <h2 className='slider__title'>Peaky Blinders</h2>
+                    <p className='slider__description'>Discover a unique style inspired by the world of Peaky Blinders! White shirt, suspenders, flat cap, stylish trousers - become a member of the Shelby family. Choose elegance and class to create your own distinctive look today! </p>
+                    <button className='button slider__btn'> Shop Now </button>
                 </div>
             </div>
         </div> 
