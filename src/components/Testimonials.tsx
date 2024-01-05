c4033c94628e8b020bce12ffac9c701f4322d8ab
Anmakulaku <anmakulaku@gmail.com>
2024-01-05 14:38:45 +0100

    Add testimonials structure

---
+++ src/components/Testimonials.tsx
@@ -0,0 +1,13 @@
+import "./Testimonials.css"
+
+export function Testimonials() {
+    return (
+        <div className="testimonials">
+            <span className="testimonials__title">This Is What Our Customers Say</span>
+            <p className="testimonials__text">Discover reviews from our customers about their experiences with our products and customer service. Find out what others have to say about our brand and see why it's worth trusting us.</p>
+            <div className="testimonials__items">
+                <div className="cardtest">Hello</div>
+            </div>
+        </div>
+    )
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,6 +4,7 @@ import { Footer } from '../components/Footer'
 import { Subscribe } from '../components/Subscribe'
 import { Gallery } from '../components/Gallery'
 import { Slider } from '../components/Slider'
+import { Testimonials } from '../components/Testimonials'
 
 export function Home() {
     return(
@@ -12,6 +13,7 @@ export function Home() {
             <Brand />
             <Slider />
             <Gallery />
+            <Testimonials />
             <Subscribe />
             <Footer />
         </div>
