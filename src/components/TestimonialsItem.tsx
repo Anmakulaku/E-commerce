bf2fa77d14a3c29e05e7076356e1239399b0b6f3
Anmakulaku <anmakulaku@gmail.com>
2024-01-08 11:00:05 +0100

    Add styles for testimonialsItem

--- src/components/TestimonialsItem.tsx
+++ src/components/TestimonialsItem.tsx
@@ -10,16 +10,21 @@ type TestimonialsItemProps = {
 
 export function TestimonialsItem ({ id, img, text, name, profession }: TestimonialsItemProps) {
     return (
+        <div className="testimonialsItem section__margin">
             <div key={id} className="testimonialsItem__container">
                 <div className="testimonialsItem__img">
+                    <div className="testimonialsItem__imgBg"></div>
                     <img src={img} alt="testimonials image" />
                 </div>
-                <div className="testimonialsItem__box">
+                <div className="testimonialsItem__infoBox">
                     <p className="testimonialsItem__text">{text}</p>
-                    <h1 className="testimonialseItem__name">{name}</h1>
+                    <div className="testimonialsItem__stars" >
+                    </div>
+                    <span className="testimonialseItem__name">{name}</span>
                     <span className="testimonialsItem__profession">{profession}</span>
                     
                 </div>
             </div>
+        </div>
     );
 }
