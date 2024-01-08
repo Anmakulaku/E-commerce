bd4bff602bcf5dbd0a92e201966194e2f9ee7a6e
Anmakulaku <anmakulaku@gmail.com>
2024-01-08 15:12:01 +0100

    Add slider to Testimonials

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -1,19 +1,47 @@
-import "./Testimonials.css"
-import { TestimonialsItem } from "../components/TestimonialsItem"
-import TestimonialsItems from "../data/itemTestimonials.json"
+import { useState } from "react";
+import "./Testimonials.css";
+import { TestimonialsItem } from "../components/TestimonialsItem";
+import TestimonialsItems from "../data/itemTestimonials.json";
+import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from "react-icons/md";
 
 export function Testimonials() {
+    const [currentSlide, setCurrentSlide] = useState(0);
+
+    const nextSlide = () => {
+        setCurrentSlide((prevSlide) => (prevSlide + 1) % TestimonialsItems.length);
+    };
+
+    const prevSlide = () => {
+        setCurrentSlide(
+            (prevSlide) => (prevSlide - 1 + TestimonialsItems.length) % TestimonialsItems.length
+        );
+    };
+
     return (
         <div className="testimonials">
-            <span className="testimonials__title">This Is What Our Customers Say</span>
-            <p className="testimonials__text">Discover reviews from our customers about their experiences with our products and customer service. Find out what others have to say about our brand and see why it's worth trusting us.</p>
-            <div className="testimonials__items">
-                {TestimonialsItems
-                .sort(() => Math.random() - 0.5) 
-                .map(item => (
-                    <TestimonialsItem key={item.id} {...item} />
-                ))}
+        <span className="testimonials__title">This Is What Our Customers Say</span>
+        <p className="testimonials__text">
+            Discover reviews from our customers about their experiences with our products and customer
+            service. Find out what others have to say about our brand and see why it's worth trusting
+            us.
+        </p>
+        <div className="testimonials__items">
+            {TestimonialsItems.map((item, index) => (
+            <TestimonialsItem
+                key={item.id}
+                {...item}
+                isVisible={index === currentSlide}
+            />
+            ))}
+        </div>
+        <div className="testimonials__arrowBox">
+            <div className="testimonials__arrow testimonials__prevArrow" onClick={prevSlide}>
+            <MdKeyboardArrowLeft />
+            </div>
+            <div className="testimonials__arrow testimonials__nextArrow" onClick={nextSlide}>
+            <MdKeyboardArrowRight />
             </div>
         </div>
-    )
+        </div>
+    );
 }
--- src/components/TestimonialsItem.tsx
+++ src/components/TestimonialsItem.tsx
@@ -6,11 +6,12 @@ type TestimonialsItemProps = {
     name: string;
     profession: string;
     img: string;
+    isVisible: boolean;
 }
 
-export function TestimonialsItem ({ id, img, text, name, profession }: TestimonialsItemProps) {
+export function TestimonialsItem ({ id, img, text, name, profession, isVisible }: TestimonialsItemProps) {
     return (
-        <div className="testimonialsItem section__margin">
+        <div className={`testimonialsItem section__margin ${isVisible ? "visible" : "hidden"}`}>
             <div key={id} className="testimonialsItem__container">
                 <div className="testimonialsItem__img">
                     <div className="testimonialsItem__imgBg"></div>
